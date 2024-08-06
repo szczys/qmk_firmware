612d72bcebda8461a4df5f6fd69a5e6975591e5a
Mike Szczys <szczys@hotmail.com>
2024-08-05 23:44:23 -0400

    remove sticky keys
    Signed-off-by: Mike Szczys <szczys@hotmail.com>

--- keyboards/crkbd/rev1/keymaps/szczys/keymap.c
+++ keyboards/crkbd/rev1/keymaps/szczys/keymap.c
@@ -86,12 +86,10 @@ void matrix_scan_user(void) {
 
 // For _QWERTY layer
 #define OSM_AGR  OSM(MOD_RALT)
-#define OSL_FUN  OSL(_FUNC)
 #define GUI_ENT  GUI_T(KC_ENT)
 #define LOW_TAB  LT(_LOWER, KC_TAB)
 #define RSE_BSP  LT(_RAISE, KC_BSPC)
 #define NUM_KCF  LT(_NUMPAD, KC_F)
-#define OSM_SFT  OSM(MOD_LSFT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT(
@@ -100,9 +98,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LALT,    KC_A,    KC_S,    KC_D, NUM_KCF,    KC_G,                     KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_QUOT ,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
- OSM(MOD_LSFT),   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                     KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,OSL_FUN ,
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                     KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,MO(_FUNC),
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LCTL, GUI_ENT, LOW_TAB,   RSE_BSP ,KC_SPC  ,OSM_SFT
+                                          KC_LCTL, GUI_ENT, LOW_TAB,   RSE_BSP ,KC_SPC  ,KC_LSFT
                                       //`--------------------------'  `--------------------------'
   ),
 
